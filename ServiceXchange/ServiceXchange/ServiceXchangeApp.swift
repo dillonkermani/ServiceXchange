1139c71ab3bc70fec6d4bf7215a1116356e161cf
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2023-01-24 09:26:55 -0800

    Adding New Xcode Project

---
+++ ServiceXchange/ServiceXchange/ServiceXchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceXchangeApp.swift
+//  ServiceXchange
+//
+//  Created by Dillon Kermani on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ServiceXchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
