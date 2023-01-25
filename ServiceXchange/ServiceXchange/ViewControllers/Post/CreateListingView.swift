685a4805120ed0af0d5af7af89bca92ab542ef5d
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2023-01-25 13:53:57 -0800

    Added ViewController Folder

---
+++ ServiceXchange/ServiceXchange/ViewControllers/Post/CreateListingView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateListingView.swift
+//  ServiceXchange
+//
+//  Created by Dillon Kermani on 1/25/23.
+//
+
+import SwiftUI
+
+struct CreateListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateListingView()
+    }
+}
