5d5229680e8e9ea1f5b3a04fc064c5c048706a0d
dillonkermani <60326614+dillonkermani@users.noreply.github.com>
2023-03-03 19:47:18 -0800

    code refactoring

--- ServiceXchange/ServiceXchange/ViewControllers/Messaging/MessageDetailView.swift
+++ ServiceXchange/ServiceXchange/ViewControllers/Messaging/MessageDetailView.swift
@@ -187,21 +187,3 @@ struct MessageDetailView_Previews: PreviewProvider {
         MessageDetailView(messagesVM: MessagesViewModel(fromUser: User(userId: "", firstName: "Sending", lastName: "User", email: "", isServiceProvider: false, listingIDs: []), toUser: User(userId: "", firstName: "Recipient", lastName: "User", email: "", isServiceProvider: false, listingIDs: [])))
     }
 }
-
-// Extension for adding rounded corners to specific corners
-extension View {
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape(RoundedCorner(radius: radius, corners: corners) )
-    }
-}
-
-// Custom RoundedCorner shape used for cornerRadius extension above
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
